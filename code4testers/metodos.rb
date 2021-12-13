eaba3c1512acae46d3b6f969b88f9b93d9fcc8c8
laylla.rodrigues <laylla.oliveira.rodrigues@hotmail.com>
2021-12-13 12:08:16 -0300

    aula_3: metodos

---
+++ code4testers/metodos.rb
@@ -0,0 +1,24 @@
+
+
+def imprime_nome(nome)
+    puts nome
+end
+
+imprime_nome("Laylla")
+
+# 1. criei o método imprime_nome
+# 2. invoquei o método para que faça a ação esperada 
+# Padrão do ruby é snake_case
+
+def soma (valor1, valor2)
+    puts valor1 + valor2
+end
+
+soma(2,3)
+
+def pega_soma(valor1,valor2)
+    valor1 + valor2
+end
+
+puts pega_soma(3,4)
+
