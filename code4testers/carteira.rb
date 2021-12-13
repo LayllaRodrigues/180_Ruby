2506c9e13f7ae2051d54fad0cfb542ef72505506
laylla.rodrigues <laylla.oliveira.rodrigues@hotmail.com>
2021-12-13 14:37:16 -0300

    aula_6: controle de fluxo

---
+++ code4testers/carteira.rb
@@ -0,0 +1,11 @@
+nome = "Laylla"
+idade = 6
+
+if (idade >= 18)
+    puts nome + ", vc pode tirar carteira de motorista."
+elsif (idade >= 7)
+    puts nome + ", continue andando de bike"
+else
+    puts nome + ", vc é um neném, ande de motoquinha =)"
+end
+    
