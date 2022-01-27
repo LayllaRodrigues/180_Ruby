9ab7513f670cff7511a22b7bc9991a09b247ddfd
laylla.rodrigues <laylla.oliveira.rodrigues@hotmail.com>
2022-01-27 15:49:12 -0300

    each

--- code4testers/loops.rb
+++ code4testers/loops.rb
@@ -7,9 +7,13 @@
 # end
 
 animais = ["Macaco", "Leão", "Arrara", "Camelo", "Arrara azul"]
-i = 0 
-animais.size.times do
-    puts animais[i]
-    i = i + 1
+# i = 0 
+# animais.size.times do
+#     puts animais[i]
+#     i = i + 1
+# end
+
+animais.each do |animal| #{uso o each para percorrer pelo array sem precisar numerar os itens}
+    puts animal
 end
 
