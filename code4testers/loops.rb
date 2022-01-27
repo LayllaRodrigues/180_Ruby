32b4abac7a4b1914dacd35bdd4c9db46fc91e237
laylla.rodrigues <laylla.oliveira.rodrigues@hotmail.com>
2022-01-27 15:42:38 -0300

    loops_time

---
+++ code4testers/loops.rb
@@ -0,0 +1,15 @@
+
+# i= 1
+
+# 5.times do 
+#     puts "execução numero #{i}"
+#     i = i +1 
+# end
+
+animais = ["Macaco", "Leão", "Arrara", "Camelo", "Arrara azul"]
+i = 0 
+animais.size.times do
+    puts animais[i]
+    i = i + 1
+end
+
