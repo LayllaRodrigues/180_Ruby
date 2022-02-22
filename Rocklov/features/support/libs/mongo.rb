39bb88e92ab3d4e4905026c887db66939d3b2dc4
laylla.rodrigues <laylla.oliveira.rodrigues@hotmail.com>
2022-02-22 10:09:04 -0300

    ajustando objetos

--- Rocklov/features/support/libs/mongo.rb
+++ Rocklov/features/support/libs/mongo.rb
@@ -7,7 +7,7 @@ class MongoDB
     def remove_user(email)
         client = Mongo::Client.new('mongodb://rocklov-db:27017/rocklov')
         users = client[:users]
-        users.delete_many({email: email})
+        users.delete_many({user: email})
     end
 
 end
