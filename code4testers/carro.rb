8c07da8fba1dbd90b7dce278fd0daa875cfc28f5
laylla.rodrigues <laylla.oliveira.rodrigues@hotmail.com>
2022-04-01 11:20:28 -0300

    classes e atribuicoes

--- code4testers/carro.rb
+++ code4testers/carro.rb
@@ -1,3 +1,28 @@
+#Uma classe é um objeto que tem caracteristicas e funcoes
+# Para atribuir caracteristicas as classes, uso o attr_accessor 
+# Para usar a classe, é necessário instancia-la, pode fazer isso com o nome_da_classe.new
+
 class carro
+    attr_accessor :nome, :cor, :marca
+
+    def ligar
+        puts "O #{@nome} está pronto para iniciar o trajeto."
+    end
 
 end
+
+chevette = Carro.new
+chevette.nome = "Chevette"
+chevette.cor = "Bege"
+chevette.marca = "GM"
+
+chevette.ligar
+puts chevette.class
+
+fusca = Carro.new
+
+fusca.nome = "Fusca"
+fusca.cor = "Azul"
+fusca.marca = "VW"
+
+fusca.ligar
