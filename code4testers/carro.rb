09ba0923513436d67c57f6fc3201a6fce16cf371
laylla.rodrigues <laylla.oliveira.rodrigues@hotmail.com>
2022-04-02 17:46:38 -0300

    refact

--- code4testers/carro.rb
+++ code4testers/carro.rb
@@ -2,7 +2,7 @@
 # Para atribuir caracteristicas as classes, uso o attr_accessor 
 # Para usar a classe, é necessário instancia-la, pode fazer isso com o nome_da_classe.new
 
-class carro
+class Carro
     attr_accessor :nome, :cor, :marca
 
     def ligar
