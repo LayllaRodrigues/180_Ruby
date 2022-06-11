4aece3b88263b5ef96d76f6c246855ad046392a4
Laylla Rodrigues <62724150+LayllaRodrigues@users.noreply.github.com>
2022-06-11 20:43:09 -0300

    adicionando rotas

--- Rocklov/api/spec/post_session_spec.rb
+++ Rocklov/api/spec/post_session_spec.rb
@@ -3,15 +3,7 @@ require "httparty"
 describe "POST /sessions" do
     context "login com sucesso" do
         before(:all) do
-         payload = { email: "laylla13@hotmail.com", password: "pwd"}
-
-         @result = HTTParty.post(
-            "http://rocklov-api:3333/sessions",
-            body: payload.to_json,
-            headers:{
-                "Content-Type": "application/json",
-             },
-            )
+         
         end
 
         it "valida status code" do
