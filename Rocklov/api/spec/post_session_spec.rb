5d07b74f7efa2cb7123b4d00ee34cd9831a41bbf
Laylla Rodrigues <62724150+LayllaRodrigues@users.noreply.github.com>
2022-06-15 22:56:21 -0300

    adc conceitos de oo

--- Rocklov/api/spec/post_session_spec.rb
+++ Rocklov/api/spec/post_session_spec.rb
@@ -1,17 +1,17 @@
-require "httparty"
+require_relative "routes/sessions"
 
-describe "POST /sessions" do
-    context "login com sucesso" do
-        before(:all) do
-         
-        end
-
-        it "valida status code" do
-            expect(@result.code).to eql 200
-        end
-
-        it "valida id do usuário" do
-            expect(@result.parsed_response["_id"].length).to eql 24
-        end
+describe "POST/sessions" do
+  context "login com sucesso" do
+    before(:all) do
+      @result = Sessions.new.login("laylla13@hotmail.com", "pwd")
     end
+
+    it "valida status code" do
+      expect(@result.code).to eql 200
+    end
+
+    it "valida id do usuário" do
+      expect(@result.parsed_response["_id"].length).to eql 24
+    end
+  end
 end
--- Rocklov/api/spec/routes/sessions.rb
+++ Rocklov/api/spec/routes/sessions.rb
@@ -1,13 +1,18 @@
+require "httparty"
+
 class Sessions
+  include HTTParty
+  base_uri "http://rocklov-api:3333"
 
-    def login(email, pass)
-        payload = { email: email, password: pass }
+  def login(email, pass)
+    payload = { email: email, password: pass }
 
-        return = HTTParty.post(
-            "http://rocklov-api:3333/sessions",
-            body: payload.to_json,
-            headers: {
-              "Content-Type": "application/json",
-            },
-        )
-    end
+    return self.class.post(
+             "/sessions",
+             body: payload.to_json,
+             headers: {
+               "Content-Type": "application/json",
+             },
+           )
+  end
+end
