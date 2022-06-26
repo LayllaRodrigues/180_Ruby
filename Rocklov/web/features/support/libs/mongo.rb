ebc8eda490bd985d5f9e18196ce19adf76939775
Laylla Rodrigues <62724150+LayllaRodrigues@users.noreply.github.com>
2022-06-26 16:36:23 -0300

    refactoring

--- Rocklov/web/features/support/libs/mongo.rb
+++ Rocklov/web/features/support/libs/mongo.rb
@@ -1,12 +1,14 @@
 require "mongo"
 
+require "mongo"
+
 Mongo::Logger.logger = Logger.new("./logs/mongo.log")
 
 class MongoDB
   attr_accessor :client, :users, :equipos
 
   def initialize
-    @client = Mongo::Client.new("mongodb://rocklov-db:27017/rocklov")
+    @client = Mongo::Client.new(CONFIG["mongo"])
     @users = client[:users]
     @equipos = client[:equipos]
   end
@@ -20,16 +22,16 @@ class MongoDB
   end
 
   def remove_user(email)
-    users.delete_many({ email: "laylla.rodrigues@hotmail.com" })
+    @users.delete_many({ email: email })
   end
 
   def get_user(email)
-    user = users.find({ email: email }).first
+    user = @users.find({ email: email }).first
     return user[:_id]
   end
 
   def remove_equipo(name, email)
-    @user_id = get_user(email)
-    equipos.delete_many({ name: name, user: @user_id })
+    user_id = get_user(email)
+    @equipos.delete_many({ name: name, user: user_id })
   end
 end
