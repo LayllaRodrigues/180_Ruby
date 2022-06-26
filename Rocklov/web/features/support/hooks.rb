64a8dc9e9ad8479813c490a0eaa3094cd4f30388
Laylla Rodrigues <62724150+LayllaRodrigues@users.noreply.github.com>
2022-06-26 16:42:02 -0300

    add hooks

--- Rocklov/web/features/support/hooks.rb
+++ Rocklov/web/features/support/hooks.rb
@@ -1,15 +1,36 @@
-# Executando ganchos antes de cada cenário para pemitir que as variaveis de intancia funcionem em todos os cenarios
+# # Executando ganchos antes de cada cenário para pemitir que as variaveis de intancia funcionem em todos os cenarios
 
-#Vantagens: redução de repetição e aumento da produtividade
+# #Vantagens: redução de repetição e aumento da produtividade
+
+# Before do
+#   @alert = Alert.new
+#   @login_page = LoginPage.new
+#   @signup_pages = SignupPage.new
+#   @dash_page = DashPage.new
+#   @equipos_page = EquiposPage.new
+
+#   # page.driver.browser.manage.window.maximize #Isso permite que ao levantar o navegador, a aba seja maximizada
+#   page.current_window.resize_to(1440,990) #ajuste exato de resolução da tela
+
+# end
 
 Before do
   @alert = Alert.new
   @login_page = LoginPage.new
-  @signup_pages = SignupPage.new
+  @signup_page = SignupPage.new
   @dash_page = DashPage.new
   @equipos_page = EquiposPage.new
 
-  # page.driver.browser.manage.window.maximize #Isso permite que ao levantar o navegador, a aba seja maximizada
-  page.current_window.resize_to(1440,990) #ajuste exato de resolução da tela
+  #page.driver.browser.manage.window.maximize
+  page.current_window.resize_to(1440, 900)
+end
 
+After do
+  temp_shot = page.save_screenshot("logs/temp_screenshot.png")
+
+  Allure.add_attachment(
+    name: "Screenshot",
+    type: Allure::ContentType::PNG,
+    source: File.open(temp_shot),
+  )
 end
